f9ffe0fb23149d62f926c068e3954791f48a009e
Kevin Haas <kev@kevhaas.com>
2020-04-27 20:57:10 -0400

    Config and build updates

--- webpack.config.d/webpack.config.js
+++ webpack.config.d/webpack.config.js
@@ -1,7 +1,1 @@
 config.resolve.modules.push("build/js/node_modules/@jetbrains")
-
-config.devServer = config.devServer || {}
-config.devServer.watchOptions = {
-    "aggregateTimeout": 5000,
-    "poll": 1000
-}
